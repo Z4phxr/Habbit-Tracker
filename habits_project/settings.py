9f032cd1ebf55ca7410307a622808e1bd7e0ad1f
kinga <kingamazur33@gmail.com>
2026-01-29 17:59:13 +0100

    Use WhiteNoise CompressedStaticFilesStorage to avoid manifest lookup issues

--- habits_project/settings.py
+++ habits_project/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'base/static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Media files (optional, for user uploads)
 MEDIA_URL = '/media/'
