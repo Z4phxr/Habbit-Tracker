9fd6e138d709bd519dc77ab38d3826e33e2b354b
kinga <kingamazur33@gmail.com>
2026-01-29 15:56:39 +0100

    Add testserver to ALLOWED_HOSTS for Django test client

--- habits_project/settings.py
+++ habits_project/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-oe+ry=g@@c!)y&h=b5$fd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+# Parse ALLOWED_HOSTS and add testserver for testing
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+# Add testserver for Django test client
+if 'testserver' not in ALLOWED_HOSTS:
+    ALLOWED_HOSTS.append('testserver')
 
 
 STATIC_URL = '/static/'
